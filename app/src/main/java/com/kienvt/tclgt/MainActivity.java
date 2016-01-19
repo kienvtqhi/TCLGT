279a8eafd0242595e0677e204a68d63bcbcd37cc
John Vi <kienvtqhi@gmail.com>
2016-01-20 00:56:22 +0700

    Init project

---
+++ app/src/main/java/com/kienvt/tclgt/MainActivity.java
@@ -0,0 +1,105 @@
+package com.kienvt.tclgt;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    Button button1;
+    Button button2;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.text_view);
+        button1 = (Button) findViewById(R.id.button_1);
+        button2 = (Button) findViewById(R.id.button_2);
+        imageView = (ImageView) findViewById(R.id.image_view);
+
+        textView.setText(R.string.app_name);
+        textView.setTextColor(Color.parseColor("#000000"));
+        textView.setVisibility(View.INVISIBLE);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setVisibility(View.VISIBLE);
+                textView.setText("Clicked on button 1");
+                Toast.makeText(MainActivity.this, "Click on button 1", Toast.LENGTH_SHORT).show();
+
+                goToSecondActivity();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setVisibility(View.INVISIBLE);
+                imageView.setBackgroundColor(Color.parseColor("#000000"));
+                Toast.makeText(MainActivity.this, "Click on button 2", Toast.LENGTH_SHORT).show();
+
+                goToThirdActivity();
+            }
+        });
+
+        Log.d("MainActivity", "onCreate");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d("MainActivity", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d("MainActivity", "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d("MainActivity", "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("MainActivity", "onResume");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d("MainActivity", "onStart");
+    }
+
+
+
+    public void goToSecondActivity() {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToThirdActivity() {
+        Intent intent = new Intent(this, ThirdActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/kienvt/tclgt/SecondActivity.java
@@ -0,0 +1,54 @@
+package com.kienvt.tclgt;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+/**
+ * Created by kienvithanh on 1/16/16.
+ */
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Log.d("StartActivity", "onCreate");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d("StartActivity", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d("StartActivity", "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d("StartActivity", "onPause");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d("StartActivity", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("StartActivity", "onResume");
+    }
+}
